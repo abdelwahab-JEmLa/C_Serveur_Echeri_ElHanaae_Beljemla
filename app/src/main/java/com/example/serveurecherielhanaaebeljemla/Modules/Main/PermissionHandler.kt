a3c63d8b7eda99b59cb15e752c3c33863ec77685
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2024-11-23 22:56:01 +0100

    //TODO Start = 0

--- app/src/main/java/com/example/serveurecherielhanaaebeljemla/Modules/Main/PermissionHandler.kt
+++ app/src/main/java/com/example/serveurecherielhanaaebeljemla/Modules/Main/PermissionHandler.kt
@@ -1,7 +1,6 @@
 package com.example.serveurecherielhanaaebeljemla.Modules.Main
 
 import android.Manifest
-import android.app.ProgressDialog.show
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
